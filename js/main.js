8b409563573a86ade1c2b7d4a903c08361da1db2
Joanna <joannaperdomo27@gmail.com>
2022-05-04 18:39:31 -0500

    al fin lo logre las animaciones

--- js/main.js
+++ js/main.js
@@ -1,35 +1,22 @@
 // Para animar botones de navegacion
 const botonesDelNavegador = document.querySelectorAll(".nav-link");
-const secciones = document.querySelectorAll("header-seccion");
+let secciones = document.querySelectorAll(".header-seccion");
 
-function cambiarEstilo (e) {
-    botonesDelNavegador.forEach(e => e.classList.remove('active'))
-    e.target.classList.add('active');
-};
-
-for (const e of botonesDelNavegador){
-    e.addEventListener('click',cambiarEstilo);
-}
-
-function cambiarEstiloNavegador () {
-    let posicionScroll = window.scrollY;
-    console.log(posicionScroll)
-    if (posicionScroll >= 800 && posicionScroll <= 1799){
+window.addEventListener('scroll', () =>{
+    let posicionScroll = window.scrollY; 
+    if (posicionScroll <= 767){
+        botonesDelNavegador.forEach(e => e.classList.remove('active'))
+    } else if (posicionScroll >= 768 && posicionScroll <= 1515){
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
         botonesDelNavegador[6].classList.add('active');
-    } else if (posicionScroll >= 1800 && posicionScroll <= 2799) {
+    } else if (posicionScroll >= 1516 && posicionScroll <= 2264) {
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
         botonesDelNavegador[7].classList.add('active');
-    } else if (posicionScroll >= 2800 && posicionScroll <= 3799){
+    } else if (posicionScroll >= 2265 && posicionScroll <= 3013){
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
         botonesDelNavegador[8].classList.add('active')
-    } else if (posicionScroll >= 3800 && posicionScroll <= 4799){
+    } else if (posicionScroll >= 3014){
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
         botonesDelNavegador[9].classList.add('active')
-    } else if (posicionScroll >= 4800 && posicionScroll <= 5799){
-        botonesDelNavegador.forEach(e => e.classList.remove('active'))
-        botonesDelNavegador[10].classList.add('active')
-    }
-}
-
-window.addEventListener('scroll', cambiarEstiloNavegador)
+    } 
+})
