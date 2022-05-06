84637457605761c04bdabe3943f571afe13d45d1
Joanna <joannaperdomo27@gmail.com>
2022-05-06 17:46:46 -0500

    añadiendo al hadita

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 // Para animar botones de navegacion
 const botonesDelNavegador = document.querySelectorAll(".nav-link");
 let secciones = document.querySelectorAll(".seccion");
+const hadita = document.getElementById('container-hadita')
 
 window.addEventListener('scroll', () =>{
     let posicionScroll = window.scrollY; 
     if (posicionScroll < secciones[0].offsetTop -100){
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
+        hadita.style.visibility = 'hidden';
     } else if (posicionScroll >= secciones[0].offsetTop -100&& posicionScroll < secciones[1].offsetTop -100){
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
         botonesDelNavegador[1].classList.add('active');
+        hadita.style.visibility = 'visible';
     } else if (posicionScroll >= secciones[1].offsetTop -100 && posicionScroll < secciones[2].offsetTop -100) {
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
         botonesDelNavegador[2].classList.add('active');
