0d5a382c2cbb8d62a8dd0a4d0ce2c6e2667bce9a
Joanna <joannaperdomo27@gmail.com>
2022-05-06 13:42:45 -0500

    nuevos cambios a la navbar

--- js/main.js
+++ js/main.js
@@ -4,20 +4,17 @@ let secciones = document.querySelectorAll(".header-seccion");
 
 window.addEventListener('scroll', () =>{
     let posicionScroll = window.scrollY; 
-    if (posicionScroll <= 767){
+    if (posicionScroll >= 768 && posicionScroll <= 1515){
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
-        botonesDelNavegador[5].classList.add('active');
-    } else if (posicionScroll >= 768 && posicionScroll <= 1515){
-        botonesDelNavegador.forEach(e => e.classList.remove('active'))
-        botonesDelNavegador[6].classList.add('active');
+        botonesDelNavegador[2].classList.add('active');
     } else if (posicionScroll >= 1516 && posicionScroll <= 2264) {
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
-        botonesDelNavegador[7].classList.add('active');
+        botonesDelNavegador[3].classList.add('active');
     } else if (posicionScroll >= 2265 && posicionScroll <= 3013){
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
-        botonesDelNavegador[8].classList.add('active')
+        botonesDelNavegador[4].classList.add('active')
     } else if (posicionScroll >= 3014){
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
-        botonesDelNavegador[9].classList.add('active')
+        botonesDelNavegador[5].classList.add('active')
     } 
 })
