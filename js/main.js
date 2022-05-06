1e1fd531bb1dd9b0c27e0eededa709856679e5de
Joanna <joannaperdomo27@gmail.com>
2022-05-06 16:35:19 -0500

    seccion nosotros

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 // Para animar botones de navegacion
 const botonesDelNavegador = document.querySelectorAll(".nav-link");
-let secciones = document.querySelectorAll(".header-seccion");
+let secciones = document.querySelectorAll(".seccion");
 
 window.addEventListener('scroll', () =>{
     let posicionScroll = window.scrollY; 
-    if (posicionScroll >= 768 && posicionScroll <= 1515){
+    if (posicionScroll < secciones[0].offsetTop -100){
+        botonesDelNavegador.forEach(e => e.classList.remove('active'))
+    } else if (posicionScroll >= secciones[0].offsetTop -100&& posicionScroll < secciones[1].offsetTop -100){
+        botonesDelNavegador.forEach(e => e.classList.remove('active'))
+        botonesDelNavegador[1].classList.add('active');
+    } else if (posicionScroll >= secciones[1].offsetTop -100 && posicionScroll < secciones[2].offsetTop -100) {
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
         botonesDelNavegador[2].classList.add('active');
-    } else if (posicionScroll >= 1516 && posicionScroll <= 2264) {
+    } else if (posicionScroll >= secciones[2].offsetTop -100 && posicionScroll < secciones[3].offsetTop -100){
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
-        botonesDelNavegador[3].classList.add('active');
-    } else if (posicionScroll >= 2265 && posicionScroll <= 3013){
+        botonesDelNavegador[3].classList.add('active')
+    } else if (posicionScroll >= secciones[3].offsetTop -100){
         botonesDelNavegador.forEach(e => e.classList.remove('active'))
         botonesDelNavegador[4].classList.add('active')
-    } else if (posicionScroll >= 3014){
-        botonesDelNavegador.forEach(e => e.classList.remove('active'))
-        botonesDelNavegador[5].classList.add('active')
     } 
 })
